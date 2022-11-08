40e16fe71ac1bca51cf2e4d97f04c7c81878159e
LV <viveiros.lucas@hotmail.com>
2022-11-08 16:12:10 +0000

    Description

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+///Flyweight Pattern
 public class ChangeColor : MonoBehaviour
 {
     private Renderer _renderer;
--- Assets/Scripts/CommandControlledBot.cs
+++ Assets/Scripts/CommandControlledBot.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
+///Command Pattern
 public class CommandControlledBot : MonoBehaviour
 {
     private NavMeshAgent _agent; 
